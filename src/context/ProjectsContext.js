d015a9e7620e060a07b7ac9ad0315ab7308cdb28
zachhco <zrocohen@gmail.com>
2024-12-19 10:45:23 -0800

    image paths

--- src/context/ProjectsContext.js
+++ src/context/ProjectsContext.js
@@ -10,8 +10,8 @@ export const ProjectsProvider = ({ children }) => {
       description: 'An Econometric Investigation into Risk Compensation & A Test of Machine Learning Model Efficacy',
       details: `MY PAPER investigates the effectiveness of seatbelts as a safety mechanism, the influence of risky behaviors like alcohol and drug consumption on accident severity, and compares the predictive performance of machine learning models against econometric models in the context of vehicle-related fatalities. I find evidence supporting the life-saving potential of seatbelts and find significant associations between substance use and increased probability of death in severe accidents. I also find that ensemble-based machine learning models, specifically the random forest approach, demonstrate superior predictive performance compared to individual econometric models such as probit and logit models in the context of predicting automotive fatalities. However, despite the predictive accuracy of the random forest model, there are interpretability challenges when compared to the econometric models, which tend to provide more straightforward interpretability due to their simpler structure and easy-to-understand coefficients.`,
       technologies: ['Machine Learning', 'Econometrics', 'Random Forest'],
-      image: 'belting3.png',
-      pdfUrl: '/resources/thesis.pdf'
+      image: process.env.PUBLIC_URL + '/images/Belting3.png',
+      pdfUrl: process.env.PUBLIC_URL + '/resources/thesis.pdf'
     },
     {
       id: 2,
@@ -19,21 +19,21 @@ export const ProjectsProvider = ({ children }) => {
       description: 'Website development for an NGO in South Africa',
       details: `During my time doing research in South Africa, I was fortunate enough to develop relationships with several amazing NGOs. One of them was Siphekela Isizwe Empowerment and Feeding Scheme. Run by Lungi, they provide nutrition, empowerment, and relief to seniors and children in western Cape townships. To give Lungi and Siphekela Isizwe Empowerment and Feeding Scheme more visibility, I created a website that provides an overview of their work and a means by which people can assist. siphekelaisizwe.com`,
       technologies: ['Web Development', 'NGO', 'South Africa'],
-      image: 'snap.png'
+      image: process.env.PUBLIC_URL + '/images/snap.png'
     },
     {
       id: 3,
       title: 'Placeholder Project 1',
       description: 'This is a placeholder project description.',
       technologies: ['Placeholder', 'Example'],
-      image: 'work.png'
+      image: process.env.PUBLIC_URL + '/images/work.png'
     },
     {
       id: 4,
       title: 'Placeholder Project 2',
       description: 'This is another placeholder project description.',
       technologies: ['Placeholder', 'Example'],
-      image: 'work.png'
+      image: process.env.PUBLIC_URL + '/images/work.png'
     }
   ]);
 
